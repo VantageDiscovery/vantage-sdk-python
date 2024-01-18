af00b4bf97890255d634bb0a2180d662c8dcd6d8
DosticJelena <jelenadostic2@gmail.com>
2024-01-18 17:51:16 +0100

    [LAB-16] Remove methods from wrapper classes

--- vantage/core/management/apis/external_api_keys.py
+++ vantage/core/management/apis/external_api_keys.py
@@ -1,12 +1,5 @@
-from pydantic import StrictStr  # type: ignore
-
 from vantage.core.base import BaseAPI
 from vantage.core.http.api.external_api_keys_api import ExternalAPIKeysApi
-from vantage.core.http.models import (
-    ExternalAPIKey,
-    ExternalAPIKeyModifiable,
-    ExternalAPIKeysResult,
-)
 
 
 __all__ = ["ExternalAPIKeysAPI"]
@@ -18,37 +11,3 @@ class ExternalAPIKeysAPI(BaseAPI):
     ):
         super().__init__(api_key, host, pool_threads)
         self.api = ExternalAPIKeysApi(api_client=self.api_client)
-
-    def get_external_api_keys(
-        self,
-        account_id: StrictStr,
-    ) -> ExternalAPIKeysResult:
-        # TODO: docstring
-        return self.api.get_external_api_keys(account_id)
-
-    def get_external_api_key(
-        self,
-        account_id: StrictStr,
-        external_key_id: StrictStr,
-    ) -> ExternalAPIKey:
-        # TODO: docstring
-        return self.api.get_external_api_key(account_id, external_key_id)
-
-    def update_external_api_key(
-        self,
-        account_id: StrictStr,
-        external_key_id: StrictStr,
-        external_api_key_modifiable: ExternalAPIKeyModifiable,
-    ) -> ExternalAPIKey:
-        # TODO: docstring
-        return self.api.update_external_api_key(
-            account_id, external_key_id, external_api_key_modifiable
-        )
-
-    def delete_external_api_key(
-        self,
-        account_id: StrictStr,
-        external_key_id: StrictStr,
-    ) -> None:
-        # TODO: docstring
-        return self.api.delete_external_api_key(account_id, external_key_id)
--- vantage/core/management/apis/vantage_api_keys.py
+++ vantage/core/management/apis/vantage_api_keys.py
@@ -1,8 +1,5 @@
-from pydantic import StrictStr  # type: ignore
-
 from vantage.core.base import BaseAPI
 from vantage.core.http.api.vantage_api_keys_api import VantageAPIKeysApi
-from vantage.core.http.models import VantageAPIKey, VantageAPIKeysResult
 
 
 __all__ = ["VantageAPIKeysAPI"]
@@ -14,15 +11,3 @@ class VantageAPIKeysAPI(BaseAPI):
     ):
         super().__init__(api_key, host, pool_threads)
         self.api = VantageAPIKeysApi(api_client=self.api_client)
-
-    def get_vantage_api_keys(
-        self, account_id: StrictStr
-    ) -> VantageAPIKeysResult:
-        # TODO: docstring
-        return self.api.get_vantage_api_keys(account_id)
-
-    def get_vantage_api_key(
-        self, account_id: StrictStr, vantage_api_key_id: StrictStr
-    ) -> VantageAPIKey:
-        # TODO: docstring
-        return self.api.get_vantage_api_key(account_id, vantage_api_key_id)
