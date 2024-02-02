8df94579b4b4796af67f219b51589f9aa68bb050
Nebojsa Arezina <nebojsa.arezina@smartcat.io>
2024-02-02 15:48:52 +0100

    [LAB-83] Add tests for semantic search

--- tests/integration_tests/test_search.py
+++ tests/integration_tests/test_search.py
@@ -113,3 +113,79 @@ class TestSearch:
 
         # Then
         assert exception.type is BadRequestException
+
+    def test_if_semantic_search_returns_result(
+        self,
+        client: Vantage,
+        account_params: dict,
+        vantage_api_key: str,
+        semantic_search_test_collection_id: str,
+    ):
+        # Given
+        collection_id = semantic_search_test_collection_id
+        accuracy = 0.5
+        search_text = "Test search"
+
+        # When
+        result = client.semantic_search(
+            text=search_text,
+            collection_id=collection_id,
+            accuracy=accuracy,
+            vantage_api_key=vantage_api_key,
+            account_id=account_params["id"],
+        )
+
+        # Then
+        assert result.status == 200
+        assert result.message == "Success."
+        assert len(result.results) == 10
+
+    def test_semantic_search_on_non_existing_collection(
+        self,
+        client: Vantage,
+        account_params: dict,
+        vantage_api_key: str,
+        random_string_generator: Callable,
+    ):
+        # Given
+        collection_id = random_string_generator(10)
+        search_text = "Test search"
+        accuracy = 0.5
+
+        # When
+        with pytest.raises(VantageNotFoundException) as exception:
+            client.semantic_search(
+                text=search_text,
+                collection_id=collection_id,
+                accuracy=accuracy,
+                vantage_api_key=vantage_api_key,
+                account_id=account_params["id"],
+            )
+
+        # Then
+        assert exception.type is VantageNotFoundException
+
+    def test_semantic_search_with_invalid_accuracy(
+        self,
+        client: Vantage,
+        account_params: dict,
+        vantage_api_key: str,
+        embedding_search_test_collection_id: str,
+    ):
+        # Given
+        collection_id = embedding_search_test_collection_id
+        search_text = "Test search"
+        accuracy = 800
+
+        # When
+        with pytest.raises(BadRequestException) as exception:
+            client.semantic_search(
+                text=search_text,
+                collection_id=collection_id,
+                accuracy=accuracy,
+                vantage_api_key=vantage_api_key,
+                account_id=account_params["id"],
+            )
+
+        # Then
+        assert exception.type is BadRequestException
