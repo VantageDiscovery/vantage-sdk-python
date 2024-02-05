3154f001253cdff6ab8a9e26bdbddb154717196e
Nebojša Arežina <nebojsa.arezina@smartcat.io>
2024-02-05 13:57:29 +0000

    [LAB-80] Add tests for external API keys

--- tests/integration_tests/test_api_keys.py
+++ tests/integration_tests/test_api_keys.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 import pytest
 
+from tests.integration_tests.conftest import skip_delete_external_api_key_test
 from vantage.core.http.exceptions import ForbiddenException, ServiceException
 from vantage.vantage import Vantage
 
@@ -10,8 +11,6 @@ from vantage.vantage import Vantage
 
 
 class TestApiKeys:
-    pass
-
     def test_get_vantage_api_keys(self, client: Vantage, account_params: dict):
         # When
         keys = client.get_vantage_api_keys(account_id=account_params["id"])
@@ -72,12 +71,187 @@ class TestApiKeys:
         client: Vantage,
         account_params: dict,
         vantage_api_key_id: str,
+        random_uuid: str,
+    ):
+        # When
+        with pytest.raises(ServiceException) as exception:
+            client.get_vantage_api_key(vantage_api_key_id=random_uuid)
+
+        # Then
+        assert exception.type is ServiceException
+
+    def test_get_external_api_keys(
+        self,
+        client: Vantage,
+        account_params: dict,
+        external_api_key: str,
+        external_api_key_id: str,
+        external_api_key_provider: str,
+    ):
+        # When
+        keys = client.get_external_api_keys(account_id=account_params["id"])
+
+        # Then
+        assert len(keys) == 1
+        api_key = keys[0]
+        assert api_key.actual_instance.account_id == account_params["id"]
+        assert api_key.actual_instance.external_key_id == external_api_key_id
+        assert (
+            api_key.actual_instance.llm_provider == external_api_key_provider
+        )
+        assert api_key.actual_instance.llm_secret == external_api_key
+
+    def test_get_external_api_key(
+        self,
+        client: Vantage,
+        account_params: dict,
+        external_api_key: str,
+        external_api_key_id: str,
+        external_api_key_provider: str,
+    ):
+        # When
+        api_key = client.get_external_api_key(
+            account_id=account_params["id"],
+            external_key_id=external_api_key_id,
+        )
+
+        # Then
+        assert api_key.account_id == account_params["id"]
+        assert api_key.external_key_id == external_api_key_id
+        assert api_key.llm_provider == external_api_key_provider
+        assert api_key.llm_secret == external_api_key
+
+    def test_get_non_existing_external_api_key(
+        self,
+        client: Vantage,
+        account_params: dict,
+        random_uuid: str,
+    ):
+        # When
+        with pytest.raises(ServiceException) as exception:
+            client.get_external_api_key(
+                account_id=account_params["id"],
+                external_key_id=random_uuid,
+            )
+
+        # Then
+        assert exception.type is ServiceException
+
+    @pytest.mark.skip(
+        reason=(
+            "Generated code for updating external api key"
+            "is not working properly."
+        )
+    )
+    def test_update_external_api_key(
+        self,
+        client: Vantage,
+        account_params: dict,
+        external_api_key_id: str,
+        random_string_generator: str,
+    ):
+        # Given
+        url = f"http://{random_string_generator(10)}"
+        llm_provider = "Hugging"
+        llm_secret = random_string_generator(10)
+        api_key = client.get_external_api_key(
+            external_key_id=external_api_key_id,
+            account_id=account_params["id"],
+        )
+        given_url = api_key.url
+        given_llm_provider = api_key.llm_provider
+        given_llm_secret = api_key.llm_secret
+
+        client.update_external_api_key(
+            external_key_id=external_api_key_id,
+            url=url,
+            llm_provider=llm_provider,
+            llm_secret=llm_secret,
+            account_id=account_params["id"],
+        )
+        # When
+        api_key = client.get_external_api_key(
+            account_id=account_params["id"],
+            external_key_id=external_api_key_id,
+        )
+
+        # Then
+        assert api_key.account_id == account_params["id"]
+        assert api_key.external_key_id == external_api_key_id
+        assert api_key.llm_provider == llm_provider
+        assert api_key.llm_secret == llm_secret
+        assert api_key.url == url
+
+        # After
+        client.update_external_api_key(
+            external_key_id=external_api_key_id,
+            url=given_url,
+            llm_provider=given_llm_provider,
+            llm_secret=given_llm_secret,
+            account_id=account_params["id"],
+        )
+
+    @pytest.mark.skip(
+        reason=(
+            "Generated code for updating external api key"
+            "is not working properly."
+        )
+    )
+    def test_update_non_existing_external_api_key(
+        self,
+        client: Vantage,
+        account_params: dict,
+        random_uuid: str,
         random_string_generator: Callable,
     ):
         # When
         with pytest.raises(ServiceException) as exception:
-            client.get_vantage_api_key(
-                vantage_api_key_id=random_string_generator(10)
+            client.update_external_api_key(
+                external_key_id=random_uuid,
+                url=random_string_generator(10),
+                llm_provider=random_string_generator(10),
+                llm_secret=random_string_generator(10),
+                account_id=account_params["id"],
+            )
+
+        # Then
+        assert exception.type is ServiceException
+
+    @pytest.mark.skipif(
+        skip_delete_external_api_key_test(), reason="Test disabled by user."
+    )
+    def test_delete_external_api_key(
+        self,
+        client: Vantage,
+        account_params: dict,
+        external_api_key_id: str,
+    ):
+        # When
+        api_key = client.delete_external_api_key(
+            external_key_id=external_api_key_id,
+            account_id=account_params["id"],
+        )
+
+        # Then
+        assert api_key is None
+        with pytest.raises(ServiceException) as exception:
+            client.get_external_api_key(
+                external_key_id=external_api_key_id,
+                account_id=account_params["id"],
+            )
+        assert exception.type is ServiceException
+
+    def test_delete_non_existing_external_api_key(
+        self,
+        client: Vantage,
+        account_params: dict,
+        random_uuid: str,
+    ):
+        # When
+        with pytest.raises(ServiceException) as exception:
+            client.delete_external_api_key(
+                external_key_id=random_uuid,
+                account_id=account_params["id"],
             )
 
         # Then
