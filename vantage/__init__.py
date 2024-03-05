6b24450fabb992cac04aee48d6e450c518d6323c
Nebojsa Arezina <nebojsa.arezina@smartcat.io>
2024-03-05 13:04:07 +0100

    [NO-ISSUE] Bump SDK version

--- vantage/__init__.py
+++ vantage/__init__.py
@@ -13,7 +13,7 @@ from vantage.vantage import VantageClient
 
 __author__ = """Vantage"""
 __email__ = 'none@vantage.com'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 __all__ = [
     "VantageClient",
     "Collection",
