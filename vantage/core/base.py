0753db767a0994980f23a806ef9460b2fea9f83e
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2024-03-28 11:58:00 +0100

    [LAB-161] Update AuthorizedApiClient with Vantage API key

--- vantage/core/base.py
+++ vantage/core/base.py
@@ -22,31 +22,30 @@ class AuthorizationClient:
         self,
         vantage_audience_url: str = DEFAULT_API_HOST,
         sso_endpoint_url: str = f"{DEFAULT_AUTH_HOST}{AUTH_ENDPOINT}",
+        vantage_api_key: Optional[str] = None,
         vantage_client_id: Optional[str] = None,
         vantage_client_secret: Optional[str] = None,
         vantage_jwt_token: Optional[str] = None,
         encoding: str = DEFAULT_ENCODING,
     ) -> None:
         self._check_credentials(
+            vantage_api_key=vantage_api_key,
             vantage_client_id=vantage_client_id,
             vantage_client_secret=vantage_client_secret,
             vantage_jwt_token=vantage_jwt_token,
         )
 
-        if vantage_jwt_token:
+        if vantage_api_key:
+            self._vantage_api_key = vantage_api_key
+        elif vantage_jwt_token:
             now = datetime.datetime.now().timestamp() * 1000
             self._jwt_token = {
                 "token": vantage_jwt_token,
                 "valid_until": now + AuthorizationClient._DAY_IN_SECONDS,
             }
-            self._user_provided_token = True
-            self._vantage_client_id = None
-            self._vantage_client_secret = None
         else:
-            self._user_provided_token = False
             self._vantage_client_id = vantage_client_id
             self._vantage_client_secret = vantage_client_secret
-            self._jwt_token = None
 
         self._vantage_audience_url = vantage_audience_url
         self._sso_endpoint_url = sso_endpoint_url
@@ -54,28 +53,23 @@ class AuthorizationClient:
 
     def _check_credentials(
         self,
+        vantage_api_key: Optional[str] = None,
         vantage_client_id: Optional[str] = None,
         vantage_client_secret: Optional[str] = None,
         vantage_jwt_token: Optional[str] = None,
     ):
-        if vantage_jwt_token and vantage_client_id and vantage_client_secret:
+        if (
+            not vantage_api_key
+            and not vantage_jwt_token
+            and not (vantage_client_id and vantage_client_secret)
+        ):
             raise ValueError(
-                "You must specify either client id and secret, or JWT token."
+                (
+                    "Please provide Vantage API key or Vantage JWT token, or both Client ID and secret. None of these was found."
+                )
             )
 
-        if not vantage_jwt_token:
-            if not (vantage_client_id and vantage_client_secret):
-                raise ValueError(
-                    (
-                        "If not using JWT token, "
-                        "both client id and secret must be specified."
-                    )
-                )
-
     def _authenticate(self) -> dict:
-        if self._user_provided_token:
-            return
-
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         body = {
             "client_id": self._vantage_client_id,
@@ -102,8 +96,9 @@ class AuthorizationClient:
         return (valid_until - 5000) - now >= 0
 
     def _get_new_token(self):
-        if self._user_provided_token:
+        if self._vantage_api_key or self._jwt_token:
             return
+
         authentication = self._authenticate()
         now = datetime.datetime.now().timestamp() * 1000
         self._jwt_token = {
@@ -113,7 +108,7 @@ class AuthorizationClient:
 
     @property
     def jwt_token(self) -> str:
-        if self._user_provided_token:
+        if self._jwt_token:
             return self._jwt_token["token"]
 
         if self._jwt_token is None or self._has_expired():
@@ -125,7 +120,7 @@ class AuthorizationClient:
         return self._jwt_token["token"]
 
     def authenticate(self):
-        if self._user_provided_token:
+        if self._vantage_api_key or self._jwt_token:
             return
 
         self._get_new_token()
@@ -135,18 +130,24 @@ class AuthorizationClient:
     @classmethod
     def automatic_token_management(
         cls,
+        vantage_api_key: str,
         vantage_client_id: str,
         vantage_client_secret: str,
         vantage_audience_url: str,
         sso_endpoint_url: str,
     ) -> AuthorizationClient:
         return cls(
+            vantage_api_key=vantage_api_key,
             vantage_client_id=vantage_client_id,
             vantage_client_secret=vantage_client_secret,
             vantage_jwt_token=None,
             vantage_audience_url=vantage_audience_url,
             sso_endpoint_url=sso_endpoint_url,
         )
+
+    @classmethod
+    def using_provided_vantage_api_key(cls, vantage_api_key: str):
+        return cls(vantage_api_key=vantage_api_key)
 
     @classmethod
     def using_provided_token(cls, vantage_jwt_token: str):
@@ -191,13 +192,16 @@ class AuthorizedApiClient(ApiClient):
             return super().call_api(*args)
 
         try:
-            header_params[
-                "authorization"
-            ] = f"Bearer {self.authorization_client.jwt_token}"
+            auth_string = (
+                self.authorization_client._vantage_api_key
+                if self.authorization_client._vantage_api_key
+                else self.authorization_client.jwt_token
+            )
+            header_params["authorization"] = f"Bearer {auth_string}"
             return super().call_api(*args)
         except UnauthorizedException:
             self.authorization_client.authenticate()
-            header_params[
-                "authorization"
-            ] = f"Bearer {self.authorization_client.jwt_token}"
+            header_params["authorization"] = (
+                f"Bearer {self.authorization_client.jwt_token}"
+            )
             return super().call_api(*args)
