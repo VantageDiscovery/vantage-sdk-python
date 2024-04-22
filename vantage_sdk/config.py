caf1d740183f88e8b4f524feef5b81061a4dcd69
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2024-04-22 16:49:10 +0200

    [LAB-205] Add upsert method implementation and new models for Documens and Metadata

--- vantage_sdk/config.py
+++ vantage_sdk/config.py
@@ -5,3 +5,6 @@ DEFAULT_AUTH_HOST = "https://auth.vanta.ge"
 AUTH_ENDPOINT = "/oauth/token"
 
 DEFAULT_ENCODING = "utf-8"
+
+METADATA_PREFIX = "meta_"
+METADATA_ORDERED_PREFIX = "meta_ordered_"
---
+++ vantage_sdk/model/document.py
@@ -0,0 +1,65 @@
+from uuid import uuid4
+from typing import Optional, List, Any, Union, Dict
+
+from pydantic import (
+    BaseModel,
+    Field,
+    StrictStr,
+    StrictFloat,
+    StrictBool,
+    StrictInt,
+    model_validator,
+)
+
+from vantage_sdk.config import METADATA_PREFIX, METADATA_ORDERED_PREFIX
+
+
+class MetadataItem(BaseModel):
+    key: StrictStr
+    value: Union[StrictStr, StrictInt, StrictFloat, StrictBool]
+
+    @model_validator(mode="before")
+    def add_prefix(cls, values: Dict):
+        key, value = values.get("key"), values.get("value")
+        if isinstance(value, float):
+            prefix = METADATA_ORDERED_PREFIX
+        else:
+            prefix = METADATA_PREFIX
+
+        if key:
+            values["key"] = prefix + key
+        return values
+
+
+class VantageDocument(BaseModel):
+    text: StrictStr
+    id: Optional[StrictStr] = Field(default_factory=lambda: uuid4().hex)
+    metadata: Optional[List[MetadataItem]] = None
+
+    def to_vantage_dict(self):
+        vantage_dict = {
+            "id": self.id,
+            "text": self.text,
+        }
+
+        if self.metadata:
+            for metadata_item in self.metadata:
+                vantage_dict[metadata_item.key] = metadata_item.value
+
+        return vantage_dict
+
+
+class VantageManagedEmbeddingsDocument(VantageDocument):
+    """Document class for Vantage-managed embeddings collections"""
+
+
+class UserProvidedEmbeddingsDocument(VantageDocument):
+    """Document class for User-provided embeddings collections"""
+
+    embeddings: List[StrictFloat] = None
+
+    def to_vantage_dict(self):
+        vantage_dict = super().to_vantage_dict()
+        vantage_dict["embeddings"] = self.embeddings
+
+        return vantage_dict
