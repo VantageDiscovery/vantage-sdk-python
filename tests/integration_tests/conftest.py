0e0b81129ed717587f53794741e5e909b3be5184
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2024-05-14 14:24:36 +0200

    [LAB-212] Add tests for upserting documents

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -2,12 +2,16 @@ import os
 import random
 import string
 import uuid
-from typing import Callable
+from typing import Callable, List
 
 import pytest
 
 from vantage_sdk.client import VantageClient
 from vantage_sdk.exceptions import VantageNotFoundError
+from vantage_sdk.model.document import (
+    UserProvidedEmbeddingsDocument,
+    VantageManagedEmbeddingsDocument,
+)
 
 
 ABS_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -322,3 +326,57 @@ def jsonl_documents_path() -> str:
 @pytest.fixture(scope="module")
 def parquet_file_path() -> str:
     return "tests/data/hello_world.parquet"
+
+
+@pytest.fixture(scope="module")
+def vantage_upe_documents() -> List[UserProvidedEmbeddingsDocument]:
+    ids = [
+        "1",
+        "2",
+        "3",
+        "4",
+    ]
+
+    texts = [
+        "First text",
+        "Second text",
+        "Third text",
+        "Fourth text",
+    ]
+
+    embeddings = [
+        [0.123, 0.234, 0.345],
+        [0.456, 0.567, 0.678],
+        [0.789, 0.891, 0.912],
+        [0.257, 0.389, 0.468],
+    ]
+    documents = [
+        UserProvidedEmbeddingsDocument(text=text, id=id, embeddings=emb)
+        for id, text, emb in zip(ids, texts, embeddings)
+    ]
+
+    return documents
+
+
+@pytest.fixture(scope="module")
+def vantage_vme_documents() -> List[VantageManagedEmbeddingsDocument]:
+    ids = [
+        "1",
+        "2",
+        "3",
+        "4",
+    ]
+
+    texts = [
+        "First text",
+        "Second text",
+        "Third text",
+        "Fourth text",
+    ]
+
+    documents = [
+        VantageManagedEmbeddingsDocument(text=text, id=id)
+        for id, text in zip(ids, texts)
+    ]
+
+    return documents
