5d8b1253873c61dcc5bf512338433de3332e8504
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2024-05-14 14:24:36 +0200

    [LAB-212] Add tests for upserting documents

--- tests/integration_tests/test_documents.py
+++ tests/integration_tests/test_documents.py
@@ -1,16 +1,82 @@
 #!/usr/bin/env bash
 
-from typing import Callable
+from typing import Callable, List
+import pytest
 
 from vantage_sdk.client import VantageClient
 from vantage_sdk.model.collection import UserProvidedEmbeddingsCollection
+from vantage_sdk.model.document import (
+    UserProvidedEmbeddingsDocument,
+    VantageManagedEmbeddingsDocument,
+)
 
 
-"""Integration tests for search endpoints"""
+"""Integration tests for document upsert endpoints"""
 
 
 class TestDocuments:
-    def test_documents_upload(
+
+    def test_user_provided_documents_upsert(
+        self,
+        client: VantageClient,
+        account_params: dict,
+        random_string_generator: Callable,
+        vantage_upe_documents: List[UserProvidedEmbeddingsDocument],
+    ):
+        """
+        TODO: docstring
+        """
+        # Given
+        collection = UserProvidedEmbeddingsCollection(
+            collection_id=random_string_generator(10),
+            embeddings_dimension=3,
+            collection_name=random_string_generator(10),
+        )
+        client.create_collection(
+            account_id=account_params["id"],
+            collection=collection,
+        )
+
+        # When
+        client.upsert_documents(
+            collection_id=collection.collection_id,
+            documents=vantage_upe_documents,
+            account_id=account_params["id"],
+        )
+
+        # Then
+        # Do nothing, if exception has not been thrown, everything is fine
+
+    def test_user_provided_documents_upsert_invalid_document_type(
+        self,
+        client: VantageClient,
+        account_params: dict,
+        random_string_generator: Callable,
+        vantage_vme_documents: List[VantageManagedEmbeddingsDocument],
+    ):
+        # Given
+        collection = UserProvidedEmbeddingsCollection(
+            collection_id=random_string_generator(10),
+            embeddings_dimension=3,
+            collection_name=random_string_generator(10),
+        )
+        client.create_collection(
+            account_id=account_params["id"],
+            collection=collection,
+        )
+
+        # When
+        with pytest.raises(ValueError) as exception:
+            client.upsert_documents(
+                collection_id=collection.collection_id,
+                documents=vantage_vme_documents,
+                account_id=account_params["id"],
+            )
+
+        # Then
+        assert exception.type is ValueError
+
+    def test_documents_upsert_from_jsonl_file(
         self,
         client: VantageClient,
         account_params: dict,
@@ -45,7 +111,7 @@ class TestDocuments:
         # Then
         # Do nothing, if exception has not been thrown, everything is fine
 
-    def test_parquet_upload(
+    def test_documents_upsert_from_parquet_file(
         self,
         client: VantageClient,
         account_params: dict,
