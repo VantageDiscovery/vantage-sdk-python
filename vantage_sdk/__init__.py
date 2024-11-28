4c7b39147710dad2b73c915423d38bb8c355f9db
Nebojsa Arezina <nebojsa.arezina@smartcat.io>
2024-11-28 14:38:00 +0100

    Set version to 0.9.6-dev1

--- vantage_sdk/__init__.py
+++ vantage_sdk/__init__.py
@@ -13,7 +13,7 @@ from vantage_sdk.model.search import (
 
 __author__ = """Vantage"""
 __email__ = 'none@vantage.com'
-__version__ = '0.9.5'
+__version__ = '0.9.6-dev1'
 __all__ = [
     "VantageClient",
     "Collection",
