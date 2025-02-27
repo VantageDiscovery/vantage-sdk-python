fc62494abdb922e3e4601d3dbc90cd8275a6d3d6
janje <nikola.jankovic@smartcat.io>
2025-02-27 14:36:00 +0100

    formatting

--- vantage_sdk/model/collection.py
+++ vantage_sdk/model/collection.py
@@ -2,7 +2,7 @@
 Models for the Collection API.
 """
 
-from typing import List, Optional, Union, Dict, Any
+from typing import Any, Dict, List, Optional, Union
 
 from pydantic import (
     BaseModel,
